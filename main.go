c14857757c27af568df7d5e5c617b08f8e627a15
Bart Meuris <b.meuris@napoleongames.be>
2021-06-07 11:47:11 +0200

    push
    Signed-off-by: Bart Meuris <b.meuris@napoleongames.be>

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
 )
 
-var body = `
+var head = `
 <html>
 <head></head>
 <body>
+`
+
+var body = `
 	<h1>Hello world!</h1>
 	From '%s%s' on '%s'
+`
+var foot = `
 </body>
 </html>
 `
@@ -21,7 +27,13 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		hn, _ := os.Hostname()
 		log.Printf("%s: %s%s", hn, r.URL.Host, r.URL.Path)
+
+		w.Write([]byte(head))
 		fmt.Fprintf(w, body, r.Host, r.URL.Path, hn)
+		if b, err := httputil.DumpRequest(r, true); err == nil {
+			w.Write(b)
+		}
+		w.Write([]byte(foot))
 	})
 	http.ListenAndServe(":8080", nil)
 }
